eb30875fa32ef30b99dc5fc5ab45122b5ddea238
Sergei Zimmerman <sergei@zimmerman.foo>
2026-07-13 00:50:56 +0300

    Make multi-call self-exec work with single file static installations
    Some things (notably the repl and a bit of other commands) need the
    ability to re-exec the nix binary with a specified argv[0] (like with
    `nix-env` etc.). This works now by looking up the symlink location in
    the directory of the `nix` binary - but it's fragile since we also want
    a single binary nix installation to work.
    
    Instead we can specify the expected argv[0] before execing. We can also
    execute /proc/self/exe directly instead of reading the magic link.
    
    This might open the door to having more internal exec-helpers for
    internal needs - like having the daemon re-exec itself for each
    connection to provide fresh ASLR entropy for each worker.

--- src/nix/upgrade-nix.cc
+++ src/nix/upgrade-nix.cc
@@ -135,9 +135,8 @@ struct CmdUpgradeNix : MixDryRun, StoreCommand
                 fmt("installing '%s' into profile %s...", store->printStorePath(storePath), PathFmt(profileDir)));
 
             // FIXME: don't call an external process.
-            runProgram(
-                getNixBin("nix-env"),
-                false,
+            runNixBin(
+                "nix-env",
                 {
                     OS_STR("--profile"),
                     profileDir.native(),
